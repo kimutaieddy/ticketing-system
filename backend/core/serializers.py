a8ac2f5a0925c8b3e80e740dc5ff4d9e4e0b7435
Kimutaieddy <eddykimutai@outlook.com>
2025-07-28 13:57:39 +0300

    Add serializers for Event and Ticket models

---
+++ backend/core/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Event, Ticket
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    event = EventSerializer(read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+        read_only_fields = ['user', 'status', 'qr_code', 'created_at']
